14c48676ae361c89216eb1eef31a7a7390ef108d
NF-coder <resetnikovs72@gmail.com>
2025-09-30 13:13:14 +0300

    improvements: better (?) for internal variables PlotUtils
    I don’t quite agree that the naming was truly incorrect. This fragment would never have to be modified because it uses well-known mathematical principles, and the new naming reduces readability

--- server/src/main/java/org/web2/controllers/services/checkers/utils/PlotUtils.java
+++ server/src/main/java/org/web2/controllers/services/checkers/utils/PlotUtils.java
@@ -2,12 +2,12 @@ package org.web2.controllers.services.checkers.utils;
 
 public class PlotUtils {
     public static PlotQuarters getQuarter(final float x, final float y) {
-        boolean isXGraterOrEqualsZero = x >= 0;
-        boolean isYGraterOrEqualsZero = y >= 0;
+        boolean xSign = x >= 0;
+        boolean ySign = y >= 0;
 
-        if (isXGraterOrEqualsZero && isYGraterOrEqualsZero) return PlotQuarters.FIRST_QUADRANT;
-        else if (isXGraterOrEqualsZero && !isYGraterOrEqualsZero) return PlotQuarters.SECOND_QUADRANT; // читабельность важнее
-        else if (!isXGraterOrEqualsZero && !isYGraterOrEqualsZero) return PlotQuarters.THIRD_QUADRANT; // читабельность важнее
+        if (xSign && ySign) return PlotQuarters.FIRST_QUADRANT;
+        else if (xSign && !ySign) return PlotQuarters.SECOND_QUADRANT; // читабельность важнее
+        else if (!xSign && !ySign) return PlotQuarters.THIRD_QUADRANT; // читабельность важнее
         return PlotQuarters.FOURTH_QUADRANT;
     }
 }
